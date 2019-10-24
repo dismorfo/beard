0fd0b3c5a04e41faedba8ab95151ee3531f6ccb5
Alberto Ortiz Flores <aortiz@nyu.edu>
2019-10-24 16:08:25 -0400

    update

---
+++ app/lambda/discovery.js
@@ -0,0 +1,15 @@
+import fetch from 'node-fetch';
+
+const { BEARD_DISCOVERY } = process.env;
+
+const q = event.queryStringParameters.q || '';
+
+exports.handler = async (event, context) => {
+  return fetch(`${BEARD_DISCOVERY}/select?q=${q}&wt=json`)
+    .then(response => response.json())
+    .then(data => ({
+      statusCode: 200,
+      body: data
+    }))
+    .catch(error => ({ statusCode: 422, body: String(error) }));
+};
